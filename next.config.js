fc8b8f9872e7de62c6a41246c4c9961ee84c2511
Éric <94077214+zaelech@users.noreply.github.com>
2025-02-03 16:36:07 +0100

    Update next.config.js to use ES modules syntax

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
+// @ts-check
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
     experimental: {
         // Ajoutez ici d'autres configurations expérimentales si nécessaire
     }
-}
+};
 
-module.exports = nextConfig
+export default nextConfig;
