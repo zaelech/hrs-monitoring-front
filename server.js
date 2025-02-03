f6ac9db1c476b5798874ac44f1149a682b684f5e
Éric <94077214+zaelech@users.noreply.github.com>
2025-02-03 15:18:26 +0100

    Ajout du serveur personnalisé pour Azure

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-const { createServer } = require("http");
-const { parse } = require("url");
-const next = require("next");
+// server.js
+import { createServer } from 'http';
+import { parse } from 'url';
+import next from 'next';
 
-const dev = process.env.NODE_ENV !== "production";
+const dev = process.env.NODE_ENV !== 'production';
+const port = process.env.PORT || 3000;
+
+// Création de l'application Next.js
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
@@ -10,8 +14,8 @@ app.prepare().then(() => {
     createServer((req, res) => {
         const parsedUrl = parse(req.url, true);
         handle(req, res, parsedUrl);
-    }).listen(process.env.PORT || 3000, (err) => {
+    }).listen(port, (err) => {
         if (err) throw err;
-        console.log("> Ready on http://localhost:" + (process.env.PORT || 3000));
+        console.log(`> Ready on http://localhost:${port}`);
     });
 });
