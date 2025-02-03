32356a38a0638f02299accd885f556647d21342f
Éric <94077214+zaelech@users.noreply.github.com>
2025-02-03 16:43:12 +0100

    Update Next.js config to properly handle @ alias

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
-// @ts-check
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
     experimental: {
         // Ajoutez ici d'autres configurations expérimentales si nécessaire
+    },
+    webpack: (config) => {
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            '@': '/app/src'  // Assurez-vous que cela pointe vers le bon chemin
+        };
+        return config;
     }
 };
 
